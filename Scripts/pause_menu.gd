5b340f82dbb2de540891fd2d86c4c12d646bb26b
Alexandre Victor <169073432+halliontk@users.noreply.github.com>
2024-08-08 11:47:59 -0300

    update02
    Exclui os point info
    Coloquei um wallpaper no Menu
    Desenhei um chãozinho aleatório com um caminho até o portal
    Coloquei um portal para interagir
    Subi alguns assets só de teste

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -24,5 +24,3 @@ func _on_menu_pressed():
 	#despausa o jogo e muda a cena para a tela inicial
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/title_screen.tscn")
-
-
--- Scripts/point_1_infos.gd
+++ Scripts/point_1_infos.gd
@@ -8,5 +8,5 @@ func _ready():
 
 func _on_a_1_pressed():
 	#quando o botão for pressionado muda a cena para a cena do diálogo
-	get_tree().change_scene_to_file("res://Scenes/point_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/point_1_infos.tscn")
 
